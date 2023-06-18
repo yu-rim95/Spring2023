5653dd8e796ebedad33a995db32c72e3779c2cca
yu-rim95 <80299209+yu-rim95@users.noreply.github.com>
2023-06-18 15:58:34 +0900

    최초

---
+++ src/test/java/com/example/yuch_ung/YuchUngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yuch_ung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuchUngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
