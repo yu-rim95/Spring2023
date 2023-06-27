54efbabf6b5722799bcf0a7e6348bb3cb7c1a40b
이충재 <knm8643@nate.com>
2023-06-27 21:08:09 +0900

    Yurim 복사본 카피

--- src/main/java/com/example/yuch_nug/comtroller/LoginController.java
+++ src/main/java/com/example/yuch_nug/comtroller/LoginController.java
@@ -4,7 +4,7 @@ import com.example.yuch_nug.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -20,6 +20,20 @@ public class LoginController {
         System.out.println("사용자가 입력한 정보는?");
         System.out.println(id + "/" + pw);
         loginService.loginSave(id, pw);
+
+        String referer = request.getHeader("Referer"); // Referer 헤더 값을 얻습니다.
+
+        if (referer != null) {
+            return "redirect:/"; // 이전 페이지로 리다이렉트합니다.
+        } else {
+            return "redirect:/index.html"; // 이전 페이지 정보가 없으면 기본 페이지로 이동합니다.
+        }
+    }
+
+    @PostMapping("/logins")
+    public String loginPage(HttpServletRequest request, String id, String pw){
+        System.out.println("사용자가 입력한 정보는?");
+        System.out.println(id + "/" + pw);
 
         String referer = request.getHeader("Referer"); // Referer 헤더 값을 얻습니다.
 
