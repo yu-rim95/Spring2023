0b0785a92896eb55e3ea2f1ae8be5602107e53e8
yu-rim95 <80299209+yu-rim95@users.noreply.github.com>
2023-06-27 20:12:14 +0900

    회원가입시 처음 페이지로 이동

--- src/main/java/com/example/yuch_nug/comtroller/LoginController.java
+++ src/main/java/com/example/yuch_nug/comtroller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class LoginController {
 
@@ -13,13 +15,19 @@ public class LoginController {
     private LoginService loginService;
 
     @GetMapping("/SiginPage")
-    @ResponseBody
-    public String SigInPage(String id, String pw){
+
+    public String SigInPage(HttpServletRequest request, String id, String pw){
         System.out.println("사용자가 입력한 정보는?");
         System.out.println(id + "/" + pw);
         loginService.loginSave(id, pw);
-        return "redirect:/index.html";
+
+        String referer = request.getHeader("Referer"); // Referer 헤더 값을 얻습니다.
+
+        if (referer != null) {
+            return "redirect:/"; // 이전 페이지로 리다이렉트합니다.
+        } else {
+            return "redirect:/index.html"; // 이전 페이지 정보가 없으면 기본 페이지로 이동합니다.
+        }
     }
-    
 
 }
